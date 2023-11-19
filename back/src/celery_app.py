d8b26026f7693f17b910ad73010d86479d43ef7f
Roman <eeuriset8@gmail.com>
2023-11-19 16:54:42 +0300

    add deserilizer

--- back/src/celery_app.py
+++ back/src/celery_app.py
@@ -14,30 +14,29 @@ from kombu import Exchange, Queue
 app = Celery(__name__)
 
 ROUTING_KEY = "1"
-      
-QUESES_NAMES = os.getenv('queue_names').split(',')
+
+QUESES_NAMES = os.getenv("queue_names").split(",")
 
 NUMBER_OF_QUEUES_FOR_NODE = len(QUESES_NAMES)
 
-EXCHANGE_TYPE = 'x-consistent-hash'
+EXCHANGE_TYPE = "x-consistent-hash"
 
-EXCHANGE_NAME = os.getenv('exchange_name')
+EXCHANGE_NAME = os.getenv("exchange_name")
 
 DEFAULT_APP_EXCHANGE = Exchange(EXCHANGE_NAME, type=EXCHANGE_TYPE)
 
 QUEUES = (Queue(name) for name in QUESES_NAMES)
 
-app.conf.task_default_exchange = 'default'
+app.conf.task_default_exchange = "default"
 app.conf.task_queues = QUEUES
 app.config_from_object(celeryconfig)
 
 with app.connection() as conn:
-
     # Create queues and exchange
     ch = conn.channel()
     DEFAULT_APP_EXCHANGE.declare(channel=ch)
     for queue in QUEUES:
         queue.declare(channel=ch)
-        queue.bind_to(channel=ch, exchange=DEFAULT_APP_EXCHANGE, routing_key=ROUTING_KEY)
-
-
+        queue.bind_to(
+            channel=ch, exchange=DEFAULT_APP_EXCHANGE, routing_key=ROUTING_KEY
+        )
--- back/src/celeryconfig.py
+++ back/src/celeryconfig.py
@@ -1,15 +1,14 @@
 import os
 
-from random import shuffle
 from dotenv import load_dotenv
 
 load_dotenv()
 
-broker_url = os.getenv('amqp_urls')
+broker_url = os.getenv("amqp_urls")
 
-result_backend = os.getenv('backend_urls')
+result_backend = os.getenv("backend_urls")
 
-imports = ['tasks']
+imports = ["tasks"]
 
 redis_backend_health_check_interval = 30
 
