be5939756ee3d0129cd70f123e992e04f1a016e5
MinyazevR <eeuriset8@gmail.com>
2023-11-25 03:27:13 +0300

    added type checking

--- back/src/test_miminet_example_works.py
+++ back/src/test_miminet_example_works.py
@@ -9,9 +9,9 @@ from tasks import mininet_worker
 class Case:
     json_network: str
     json_answer: str
-    pattern_in_network: str = r''
+    pattern_in_network: str = r""
     pattern_len: int = 0
-    pattern_for_replace: str = r''
+    pattern_for_replace: str = r""
 
 
 DEFAULT_JSON_TEST_DIRECTORY = "test_json/"
@@ -28,14 +28,31 @@ FILE_NAMES = [
     ("switch_and_hub_network.json", "switch_and_hub_answer.json"),
     ("router_network.json", "router_answer.json"),
     ("first_and_last_ip_address_network.json", "first_and_last_ip_address_answer.json"),
-   
 ]
 
 DINAMYC_PORT_FILE_NAMES = [
-    ("tcp_connection_setup_1_network.json", "tcp_connection_setup_1_answer.json", r'TCP \(SYN\) \d+', len('TCP (SYN) '), r'port'),
-    ("icmp_network_unavailable_network.json", "icmp_network_unavailable_answer.json", r'TCP \(SYN\) \d+', len('TCP (SYN) '), r'port'),
-    ("icmp_host_unreachable_network.json", "icmp_host_unreachable_answer.json", r'TCP \(SYN\) \d+', len('TCP (SYN) '), r'port'),
-    #("multicast_udp_traffic_network.json", "multicast_udp_traffic_answer.json", r'UDP \d+', len('UDP '), r'port'),
+    (
+        "tcp_connection_setup_1_network.json",
+        "tcp_connection_setup_1_answer.json",
+        r"TCP \(SYN\) \d+",
+        len("TCP (SYN) "),
+        r"port",
+    ),
+    (
+        "icmp_network_unavailable_network.json",
+        "icmp_network_unavailable_answer.json",
+        r"TCP \(SYN\) \d+",
+        len("TCP (SYN) "),
+        r"port",
+    ),
+    (
+        "icmp_host_unreachable_network.json",
+        "icmp_host_unreachable_answer.json",
+        r"TCP \(SYN\) \d+",
+        len("TCP (SYN) "),
+        r"port",
+    ),
+    # ("multicast_udp_traffic_network.json", "multicast_udp_traffic_answer.json", r'UDP \d+', len('UDP '), r'port'),
 ]
 
 TEST_CASES = [
@@ -45,8 +62,12 @@ TEST_CASES = [
 
 DINAMYC_PORT_TEST_CASES = [
     Case(network, answer, pattern, length, replace)
-    for (network, answer, pattern, length, replace) in [list(read_files(case[0], case[1])) + [case[2], case[3], case[4]] for case in DINAMYC_PORT_FILE_NAMES]
+    for (network, answer, pattern, length, replace) in [
+        list(read_files(case[0], case[1])) + [case[2], case[3], case[4]]
+        for case in DINAMYC_PORT_FILE_NAMES
+    ]
 ]
+
 
 @pytest.mark.parametrize("test", TEST_CASES)
 def test_miminet_work(test: Case) -> None:
@@ -54,14 +75,15 @@ def test_miminet_work(test: Case) -> None:
     animation = re.sub(r'"timestamp": "\d+"', r'"timestamp": ""', animation)
     animation = re.sub(r'"id": "\w+"', r'"id": ""', animation)
     assert animation == test.json_answer
-    
+
+
 @pytest.mark.parametrize("test", DINAMYC_PORT_TEST_CASES)
 def test_miminet_work_for_dinamyc_port_test_cases(test: Case) -> None:
     animation, pcaps = mininet_worker(test.json_network)
     animation = re.sub(r'"timestamp": "\d+"', r'"timestamp": ""', animation)
     animation = re.sub(r'"id": "\w+"', r'"id": ""', animation)
     port_string = re.search(test.pattern_in_network, animation)
-    port = port_string.group(0)[test.pattern_len:]
+    assert port_string is not None
+    port = port_string.group(0)[test.pattern_len :]
     test.json_answer = re.sub(test.pattern_for_replace, port, test.json_answer)
     assert animation == test.json_answer
-    
