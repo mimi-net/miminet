b11dc3e9b68f933ffc00da09cd2dee0f1640ac54
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2023-12-23 11:19:56 +0300

    Change Vagrant and celery configuration (#41)
    * Change Vagrant configuration
    
    * formatted

--- front/src/celery_app.py
+++ front/src/celery_app.py
@@ -23,8 +23,3 @@ EXCHANGE_NAME = os.getenv("exchange_name")
 DEFAULT_APP_EXCHANGE = Exchange(EXCHANGE_NAME, type=EXCHANGE_TYPE)
 
 app.config_from_object(celeryconfig)
-
-with app.connection() as conn:
-    # Create queues and exchange
-    ch = conn.channel()
-    DEFAULT_APP_EXCHANGE.declare(channel=ch)
