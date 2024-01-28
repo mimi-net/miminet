9ff5d37259b08a3378810b226aea60020b0a915d
MinyazevR <eeuriset8@gmail.com>
2024-01-28 16:29:54 +0300

    Removing unnecessary dependencies

--- back/src/tasks.py
+++ back/src/tasks.py
@@ -1,24 +1,20 @@
 import json
 import os
 import signal
-import typing
-
 import marshmallow_dataclass
-import redis
-from celery import shared_task
+from celery_app import (
+    app,
+    SEND_NETWORK_RESPONSE_EXCHANGE,
+    SEND_NETWORK_RESPONSE_ROUTING_KEY,
+)
 from mininet.log import setLogLevel
 
 from network import Network
 from simulate import run_mininet
 
 
-@shared_task(
-    autoretry_for=(redis.exceptions.ConnectionError,),
-    retry_kwargs={"max_retries": 3, "countdown": 5},
-)
-def mininet_worker(
-    network: str,
-) -> tuple[str, list[typing.Any] | list[tuple[str, bytes]]]:
+@app.task(bind=True)
+def mininet_worker(self, network: str):
     """Worker for start mininet simulation
 
     Args:
@@ -48,4 +44,18 @@ def mininet_worker(
             break
         else:
             break
-    return json.dumps(animation), pcaps
+
+    network_task = self.request.headers["network_task_name"]
+    task_id = self.request.id
+
+    app.send_task(
+        network_task,
+        (
+            json.dumps(animation),
+            pcaps,
+        ),
+        routing_key=SEND_NETWORK_RESPONSE_ROUTING_KEY,
+        exchange=SEND_NETWORK_RESPONSE_EXCHANGE.name,
+        exchange_type=SEND_NETWORK_RESPONSE_EXCHANGE.type,
+        task_id=task_id,
+    )
--- front/src/miminet_simulation.py
+++ front/src/miminet_simulation.py
@@ -1,9 +1,11 @@
 import os
-import shutil
 import uuid
 
-from celery.result import AsyncResult
-from celery_app import DEFAULT_APP_EXCHANGE, EXCHANGE_TYPE, app
+from celery_app import (
+    SEND_NETWORK_EXCHANGE,
+    EXCHANGE_TYPE,
+    app,
+)
 from flask import jsonify, make_response, redirect, request, url_for
 from flask_login import current_user, login_required
 from miminet_model import Network, Simulate, SimulateLog, db
@@ -39,29 +41,27 @@ def run_simulation():
             db.session.delete(s)
             db.session.commit()
 
-        task_id = str(uuid.uuid4())
-
         simlog = SimulateLog(
             author_id=net.author_id, network=net.network, network_guid=net.guid
         )
 
-        sim = Simulate(network_id=net.id, packets="", task_guid=task_id)
+        task_guid = uuid.uuid4()
+        sim = Simulate(network_id=net.id, packets="", task_guid=str(task_guid))
         db.session.add(sim)
         db.session.add(simlog)
-        db.session.flush()
-        db.session.refresh(sim)
         db.session.commit()
 
         app.send_task(
             "tasks.mininet_worker",
             (net.network,),
             routing_key=str(uuid.uuid4()),
-            exchange=DEFAULT_APP_EXCHANGE,
+            exchange=SEND_NETWORK_EXCHANGE,
             exchange_type=EXCHANGE_TYPE,
-            task_id=task_id,
+            task_id=str(task_guid),
+            headers={"network_task_name": "tasks.save_simulate_result"},
         )
 
-        ret = {"simulation_id": task_id}
+        ret = {"simulation_id": str(task_guid)}
         return make_response(jsonify(ret), 201)
 
     return redirect(url_for("home"))
@@ -73,61 +73,32 @@ def check_simulation():
     network_guid = request.args.get("network_guid", type=str)
 
     if not task_guid:
-        ret = {"message": "Пропущен параметр task_id."}
+        ret = {"message": "Пропущен параметр task_guid."}
         return make_response(jsonify(ret), 400)
 
     if not network_guid:
         ret = {"message": "Пропущен параметр network_guid."}
         return make_response(jsonify(ret), 400)
 
-    try:
-        task_result = AsyncResult(task_guid)
-        if task_result.status != "SUCCESS" and task_result.status != "FAILURE":
-            return make_response(jsonify({"message": "Сеть в процессе симуляции"}), 210)
-    except Exception:
-        return make_response(
-            jsonify({"message": "Ошибка при подключении к backend серверу"}), 400
-        )
-
-    try:
-        result = task_result.result
-    except Exception:
-        return make_response(
-            jsonify({"message": "Ошибка на стороне ipmininet worker"}), 400
-        )
-
-    packets, binary_pcaps = result
-
-    # Add result main storage
     sim = Simulate.query.filter(Simulate.task_guid == task_guid).first()
-    simlog = (
-        SimulateLog.query.filter(SimulateLog.network_guid == network_guid)
-        .order_by(SimulateLog.id.desc())
-        .first()
-    )
-    sim.packets = packets
-    sim.ready = True
-    if simlog:
-        if not simlog.ready:
-            simlog.ready = True
 
-    db.session.commit()
+    if not sim:
+        ret = {"message": "Нет такой симуляции"}
+        return make_response(jsonify(ret), 400)
 
-    # Check for a pcaps
-    pcap_dir = "static/pcaps/" + network_guid
+    if sim.ready:
+        pcap_dir = "static/pcaps/" + network_guid
+        pcaps = []
 
-    if not os.path.exists(pcap_dir):
-        os.makedirs(pcap_dir)
-    else:
-        shutil.rmtree(pcap_dir)
-        os.makedirs(pcap_dir)
+        if os.path.exists(pcap_dir):
+            pcaps = [
+                os.path.splitext(f)[0]
+                for f in os.listdir(pcap_dir)
+                if os.path.isfile(os.path.join(pcap_dir, f))
+            ]
 
-    pcaps = []
-    for pcap, name in binary_pcaps:
-        pcaps += name
-        with open(pcap_dir + "/" + name + ".pcap", "wb") as file:
-            file.write(pcap)
+        ret = {"message": "Симуляция завершена", "packets": sim.packets, "pcaps": pcaps}
+        return make_response(jsonify(ret), 200)
 
-    ret = {"message": "Симуляция завершена", "packets": packets, "pcaps": pcaps}
-
-    return make_response(jsonify(ret), 200)
+    ret = {"message": "Сеть в процессе симуляции"}
+    return make_response(jsonify(ret), 210)
