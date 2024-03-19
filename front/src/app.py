8a15bd4d92a98d873dd8385a633261fc89d4fb45
ElenaBakova <lenabakova72@gmail.com>
2024-03-20 00:59:49 +0300

    Removed unused endpoints

--- front/src/app.py
+++ front/src/app.py
@@ -59,18 +59,12 @@ from quiz.controller.quiz_session_controller import (
     get_results_by_user_endpoint,
 )
 from quiz.controller.section_controller import (
-    create_section_endpoint,
     get_sections_by_test_endpoint,
 )
 from quiz.controller.test_controller import (
-    create_test_endpoint,
     get_all_tests_endpoint,
     get_tests_by_owner_endpoint,
-    get_deleted_tests_by_owner_endpoint,
-    delete_test_endpoint,
-    edit_test_endpoint,
     get_test_endpoint,
-    publish_or_unpublish_test_endpoint,
 )
 from quiz.entity.entity import Section, Test, Question, Answer
 
@@ -155,19 +149,7 @@ app.add_url_rule(
     "/quiz/test/owner", methods=["GET"], view_func=get_tests_by_owner_endpoint
 )
 app.add_url_rule("/quiz/test/all", methods=["GET"], view_func=get_all_tests_endpoint)
-# app.add_url_rule(
-#     "/quiz/test/owner/deleted",
-#     methods=["GET"],
-#     view_func=get_deleted_tests_by_owner_endpoint,
-# )
-# app.add_url_rule(
-#     "/quiz/test/delete", methods=["DELETE"], view_func=delete_test_endpoint
-# )
-# app.add_url_rule("/quiz/test/edit", methods=["PUT"], view_func=edit_test_endpoint)
 app.add_url_rule("/quiz/test/get", methods=["GET"], view_func=get_test_endpoint)
-# app.add_url_rule(
-#     "/quiz/test/publish", methods=["PUT"], view_func=publish_or_unpublish_test_endpoint
-# )
 
 app.add_url_rule(
     "/quiz/section/test/all", methods=["GET"], view_func=get_sections_by_test_endpoint
@@ -176,9 +158,6 @@ app.add_url_rule(
 app.add_url_rule(
     "/quiz/question/create", methods=["POST"], view_func=create_question_endpoint
 )
-# app.add_url_rule(
-#     "/quiz/question/delete", methods=["DELETE"], view_func=delete_question_endpoint
-# )
 app.add_url_rule(
     "/quiz/question/all", methods=["GET"], view_func=get_questions_by_section_endpoint
 )
