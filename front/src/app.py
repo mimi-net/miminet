65ec88e4d43736317f33d6a78d1978f2b2e08445
ElenaBakova <lenabakova72@gmail.com>
2024-04-12 14:47:02 +0300

    Added practice question deletion API

--- front/src/app.py
+++ front/src/app.py
@@ -49,7 +49,7 @@ from miminet_shark import mimishark_page
 from miminet_simulation import check_simulation, run_simulation
 from quiz.controller.question_controller import (
     get_questions_by_section_endpoint,
-    create_question_endpoint,
+    create_question_endpoint, delete_question_endpoint,
 )
 from quiz.controller.quiz_session_controller import (
     start_session_endpoint,
@@ -65,8 +65,7 @@ from quiz.controller.section_controller import (
 from quiz.controller.test_controller import (
     get_all_tests_endpoint,
     get_tests_by_owner_endpoint,
-    get_test_endpoint,
-)
+    get_test_endpoint, )
 from quiz.entity.entity import Section, Test, Question, Answer
 
 app = Flask(
@@ -160,6 +159,11 @@ app.add_url_rule(
 app.add_url_rule(
     "/quiz/question/create", methods=["POST"], view_func=create_question_endpoint
 )
+
+app.add_url_rule(
+    "/quiz/question/delete", methods=["DELETE"], view_func=delete_question_endpoint
+)
+
 app.add_url_rule(
     "/quiz/question/all", methods=["GET"], view_func=get_questions_by_section_endpoint
 )
