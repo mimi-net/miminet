25a3be8e7408b30f9e7de947ce80dbb60211f8ac
Ilya Zelenchuk <ilya@hackerdom.ru>
2024-10-10 16:54:44 +0300

    Ilya adds packets info (#150)
    * Добавил описание для ARP и STP пакетов, когда они отображаются в анимации
    
    * Fix linter warnings
    
    * Fix linter warnings #2
    
    * Refactor shark_script.js and add STP decoder
    
    * Fix hub and switch test
    
    * Fix #2 switch_and_hub_answer.json
    
    * Fix #3 switch_and_hub_answer.json
    
    * first_and_last_ip_address_answer.json and router_answer.json
    
    * Fix first and last ip adress answer
    
    * Try fix test #1
    
    * Try fix test #2
    
    * Try fix test #3
    
    * Try fix test #4
    
    * Try fix test #5
    
    * Try fix test #6
    
    * Try fix test #7
    
    * Try fix test #8
    
    * Try fix test #9
    
    * Try fix test #10
    
    * Try fix test #11
    
    * Try fix test #12
    
    * Try fix test #13

--- back/src/test_miminet_example_works.py
+++ back/src/test_miminet_example_works.py
@@ -28,7 +28,6 @@ def read_files(network_filename: str, answer_filename: str):
 
 FILE_NAMES = [
     ("switch_and_hub_network.json", "switch_and_hub_answer.json"),
-    ("router_network.json", "router_answer.json"),
     ("first_and_last_ip_address_network.json", "first_and_last_ip_address_answer.json"),
     ("vlan_access_network.json", "vlan_access_answer.json"),
     ("vlan_trunk_network.json", "vlan_trunk_answer.json"),
@@ -44,6 +43,20 @@ DINAMYC_PORT_FILE_NAMES = [
         len("TCP (SYN) "),
         r"port",
     ),
+]
+
+DINAMYC_ARP_FILE_NAMES = [
+    (
+        "router_network.json",
+        "router_answer.json",
+        r"ARP-response\\n.+ at ([0-9a-fA-F]{2}[:]){6}",
+        len("ARP-response\\n10.0.0.1 at "),
+        r"mac",
+    ),
+]
+
+
+DINAMYC_ARP_AND_PORT_FILE_NAMES = [
     (
         "icmp_network_unavailable_network.json",
         "icmp_network_unavailable_answer.json",
@@ -58,7 +71,6 @@ DINAMYC_PORT_FILE_NAMES = [
         len("TCP (SYN) "),
         r"port",
     ),
-    # ("multicast_udp_traffic_network.json", "multicast_udp_traffic_answer.json", r'UDP \d+', len('UDP '), r'port'),
 ]
 
 EXCLUDE_PACKETS_FILE_NAMES = [
@@ -77,6 +89,22 @@ DINAMYC_PORT_TEST_CASES = [
     for (network, answer, pattern, length, replace) in [
         list(read_files(case[0], case[1])) + [case[2], case[3], case[4]]
         for case in DINAMYC_PORT_FILE_NAMES
+    ]
+]
+
+DINAMYC_ARP_AND_PORT_TEST_CASES = [
+    Case(network, answer, pattern, length, replace)
+    for (network, answer, pattern, length, replace) in [
+        list(read_files(case[0], case[1])) + [case[2], case[3], case[4]]
+        for case in DINAMYC_ARP_AND_PORT_FILE_NAMES
+    ]
+]
+
+DINAMYC_ARP_TEST_CASES = [
+    Case(network, answer, pattern, length, replace)
+    for (network, answer, pattern, length, replace) in [
+        list(read_files(case[0], case[1])) + [case[2], case[3], case[4]]
+        for case in DINAMYC_ARP_FILE_NAMES
     ]
 ]
 
@@ -137,6 +165,32 @@ def test_miminet_work_for_dinamyc_port_test_cases(test: Case) -> None:
     assert port_string is not None
     port = port_string.group(0)[test.pattern_len :]
     test.json_answer = re.sub(test.pattern_for_replace, port, test.json_answer)
+    assert animation == test.json_answer
+
+
+@pytest.mark.parametrize("test", DINAMYC_ARP_AND_PORT_TEST_CASES)
+def test_miminet_work_for_dinamyc_arp_and_port_test_cases(test: Case) -> None:
+    animation, pcaps = simulate(test.json_network)
+    animation = re.sub(r'"timestamp": "\d+"', r'"timestamp": ""', animation)
+    animation = re.sub(r'"id": "\w+"', r'"id": ""', animation)
+    animation = re.sub(
+        r'"ARP-response.+? at .+?"', r'"ARP-response"', animation, flags=re.S
+    )
+    port_string = re.search(test.pattern_in_network, animation)
+    assert port_string is not None
+    port = port_string.group(0)[test.pattern_len :]
+    test.json_answer = re.sub(test.pattern_for_replace, port, test.json_answer)
+    assert animation == test.json_answer
+
+
+@pytest.mark.parametrize("test", DINAMYC_ARP_TEST_CASES)
+def test_miminet_work_for_dinamyc_arp_test_cases(test: Case) -> None:
+    animation, pcaps = simulate(test.json_network)
+    animation = re.sub(r'"timestamp": "\d+"', r'"timestamp": ""', animation)
+    animation = re.sub(r'"id": "\w+"', r'"id": ""', animation)
+    animation = re.sub(
+        r'"ARP-response.+? at .+?"', r'"ARP-response"', animation, flags=re.S
+    )
     assert animation == test.json_answer
 
 
