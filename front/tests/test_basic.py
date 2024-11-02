cd9a458aa75b088f69dd3aac6d695e5424ca0c58
d-zaytsev <dzaytsev308@gmail.com>
2024-11-02 13:47:59 +0000

    Add comments & types

--- front/tests/test_basic.py
+++ front/tests/test_basic.py
@@ -1,21 +1,24 @@
 import pytest
 from conftest import testing_settings
 from selenium.webdriver.common.by import By
+from requests import Session
+from selenium.webdriver import Chrome
+
 
 class TestAvailability:
 
-    def test_auth(self, selenium):
-        selenium.get(testing_settings.miminet_address)
-        selenium.find_element(By.XPATH, "/html/body/nav/div/div/li[3]").click()
+    def test_auth(self, selenium: Chrome, main_page: str):
+        """Checks if it possible to open home page (are we authorized or not)"""
+        selenium.get(f'{main_page}/home')
 
         assert selenium.title == "Веб-эмулятор"
-        assert selenium.current_url == testing_settings.miminet_address + "/home"
 
     @pytest.mark.parametrize(
         "endpoint", ["/", "/auth/login.html", "/quiz/test/all", "/examples", "/home"]
     )
-    def test_pages_availability(self, testing_session, endpoint):
-        url = f"{testing_settings.miminet_address}{endpoint}"
-        status_code = testing_session.get(url).status_code
+    def test_pages_availability(self, endpoint: str, requester: Session, main_page: str):
+        """Checks accessibility for specified pages"""
+        url = f"{main_page}{endpoint}"
+        status_code = requester.get(url).status_code
 
         assert status_code == 200
