e36a1996ca93c7b145dc7b2ed26a72b45ec42273
d-zaytsev <dzaytsev308@gmail.com>
2024-11-02 13:49:32 +0000

    Black fmt

--- front/tests/test_basic.py
+++ front/tests/test_basic.py
@@ -9,14 +9,16 @@ class TestAvailability:
 
     def test_auth(self, selenium: Chrome, main_page: str):
         """Checks if it possible to open home page (are we authorized or not)"""
-        selenium.get(f'{main_page}/home')
+        selenium.get(f"{main_page}/home")
 
         assert selenium.title == "Веб-эмулятор"
 
     @pytest.mark.parametrize(
         "endpoint", ["/", "/auth/login.html", "/quiz/test/all", "/examples", "/home"]
     )
-    def test_pages_availability(self, endpoint: str, requester: Session, main_page: str):
+    def test_pages_availability(
+        self, endpoint: str, requester: Session, main_page: str
+    ):
         """Checks accessibility for specified pages"""
         url = f"{main_page}{endpoint}"
         status_code = requester.get(url).status_code
