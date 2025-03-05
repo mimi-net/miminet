3b100847fa6db7cb1e7508b91bb0c80f81022fdd
Ivan Shurekov <ivan.shurenkov@yandex.com>
2025-03-05 10:19:22 +0300

    Fix start ovs

--- back/ENTRYPOINT.sh
+++ back/ENTRYPOINT.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 /usr/share/openvswitch/scripts/ovs-ctl start
+ovs-vswitchd &
 
 exec python3 -m celery -A celery_app worker --loglevel=info --concurrency=${celery_concurrency} -Q ${queue_names}
