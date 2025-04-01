d48102bc0c8bef8fa2668456ba1129c59dd5d15a
d-zaytsev <dzaytsev308@gmail.com>
2025-04-01 07:01:41 +0000

    Fix tasks.py empty emulation result

--- back/src/tasks.py
+++ back/src/tasks.py
@@ -40,11 +40,11 @@ def simulate(network: str):
             animation, pcaps = run_mininet(network)
 
             return json.dumps(animation), pcaps
-        except ValueError:
-            continue
         except Exception as e:
+            # Sometimes mininet doesn't work correctly and simulation needs to be redone,
+            # Example of mininet error: https://github.com/mininet/mininet/issues/737.
             error(e)
-            break
+            continue
 
     return "", []
 
