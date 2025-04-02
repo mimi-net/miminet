46689eafa58525547bf7290f32da001b13f952c6
RamProsad2 <ramprosad.chandrasutradhar@gmail.com>
2025-04-02 13:07:45 +0300

    update

--- back/src/net_utils/vlan.py
+++ back/src/net_utils/vlan.py
@@ -41,10 +41,20 @@ def clean_bridges(net: IPNet) -> None:
         switch.cmd(f'brctl delbr {f"br-{switch.name}"}')
 
 
+
 def configure_access(switch: IPSwitch, intf: str, vlan: int) -> None:
     switch.cmd(f'ip link set {intf} master {f"br-{switch.name}"}')
     switch.cmd(f"bridge vlan del dev {intf} vid 1")
     switch.cmd(f"bridge vlan add dev {intf} vid {vlan} pvid untagged")
+    
+    # Enable ARP Proxy on this interface
+    switch.cmd(f"sysctl -w net.ipv4.conf.{intf}.proxy_arp=1")
+    switch.cmd(f"sysctl -w net.ipv4.conf.{intf}.forwarding=1")  # Enable forwarding
+
+    # Enable ARP Proxy for VLAN sub-interface (if created)
+    sub_intf = f"{intf}.{vlan}"
+    switch.cmd(f"sysctl -w net.ipv4.conf.{sub_intf}.proxy_arp=1")
+    switch.cmd(f"sysctl -w net.ipv4.conf.{sub_intf}.forwarding=1")
 
 
 def configure_trunk(switch: IPSwitch, intf: str, vlans: list[int]) -> None:
@@ -54,8 +64,21 @@ def configure_trunk(switch: IPSwitch, intf: str, vlans: list[int]) -> None:
     for vlan in vlans:
         switch.cmd(f"bridge vlan add dev {intf} vid {vlan}")
 
+        # Enable ARP Proxy for each VLAN sub-interface on the trunk
+        sub_intf = f"{intf}.{vlan}"
+        switch.cmd(f"sysctl -w net.ipv4.conf.{sub_intf}.proxy_arp=1")
+        switch.cmd(f"sysctl -w net.ipv4.conf.{sub_intf}.forwarding=1")
 
+    # Enable ARP Proxy on the trunk interface itself
+    switch.cmd(f"sysctl -w net.ipv4.conf.{intf}.proxy_arp=1")
+    switch.cmd(f"sysctl -w net.ipv4.conf.{intf}.forwarding=1")  
+
+   
 def add_bridge(switch: IPSwitch) -> None:
     switch.cmd(f'ip link add name {f"br-{switch.name}"} type bridge')
     switch.cmd(f'ip link set dev {f"br-{switch.name}"} up')
     switch.cmd(f'ip link set dev {f"br-{switch.name}"} type bridge vlan_filtering 1')
+
+    # Enable ARP Proxy for the bridge
+    switch.cmd(f"sysctl -w net.ipv4.conf.{f'br-{switch.name}'}.proxy_arp=1")
+    switch.cmd(f"sysctl -w net.ipv4.conf.{f'br-{switch.name}'}.forwarding=1")
