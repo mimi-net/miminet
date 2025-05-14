f3cd5a5220f509a055a273c25ee121cc5a8a8e4f
d-zaytsev <dzaytsev308@gmail.com>
2025-05-14 12:05:38 +0000

    Fix format bug

--- front/src/miminet_network.py
+++ front/src/miminet_network.py
@@ -15,6 +15,7 @@ from flask import (
 from flask_login import current_user, login_required
 from miminet_config import check_image_with_pil
 from miminet_model import Network, Simulate, db, SimulateLog
+import datetime
 
 
 @login_required
@@ -528,7 +529,7 @@ def get_last_emulation_time():
         return make_response(jsonify({"message": "Никаких эмуляций не найдено."}), 404)
 
     return make_response(
-        jsonify({"time": last_emulation_time}),
+        jsonify({"time": str(last_emulation_time.isoformat())}),
         200,
     )
 
@@ -536,7 +537,8 @@ def get_last_emulation_time():
 @login_required
 def get_emulation_queue_size():
     """Answer with current emulation queue size filtered by emulation time."""
-    time_filter = request.args.get("time-filter", type=str)
+    time_filter_req: str = request.args.get("time-filter", type=str)
+    time_filter: datetime.datetime = datetime.datetime.fromisoformat(time_filter_req)
 
     if not time_filter:
         return make_response(
@@ -545,7 +547,7 @@ def get_emulation_queue_size():
 
     emulated_networks_count = (
         SimulateLog.query.filter(SimulateLog.ready == 0)
-        .filter(SimulateLog.simulate_start < time_filter)
+        .filter(SimulateLog.simulate_start <= time_filter)
         .count()
     )
 
