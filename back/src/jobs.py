8ecf85ab200ca4500f4f98d0a0405c562b4bba07
Ivan Shurekov <31987412+ioannessh@users.noreply.github.com>
2025-11-26 18:04:33 +0300

    Добавил интерфейсы для DHCP (#365)
    * Добавил обновление pip
    
    * Добавил определение типов DHCP пакетов
    
    * Добавил выбор интерфейсов для DHCP на фронте
    
    * Выбор интерфейсов Dnsmasq на back
    
    * Поправил работу dhclient
    
    * Поправил тесты для DHCP
    
    * Изменил версию ipmininet
    
    ---------
    
    Co-authored-by: Шуренков Иван <ishurenkov@naumen.ru>
    Co-authored-by: Ilya Zelenchuk <ilya@hackerdom.ru>

--- back/src/jobs.py
+++ back/src/jobs.py
@@ -417,11 +417,11 @@ def arp_proxy_enable(job: Job, job_host: Any) -> None:
 
 
 def dhcp_client(job: Job, job_host):
-    job_host.cmd(f"ifconfig {job_host.intf().name} 0")
+    job_host.cmd(f"ifconfig {job.arg_1} 0")
     job_host.cmd("rm /var/lib/dhcp/dhclient.leases")
+    job_host.cmd("echo 'initial-interval 6;' > /tmp/dhclient.conf")
     out = job_host.cmd(
-        "echo 'initial-interval 5;' > /tmp/dhclient.conf && "
-        + f"timeout -k 0 6 dhclient -v -4 -cf /tmp/dhclient.conf {job_host.intf().name} && "
+        f"timeout -k 1 5 dhclient -d -v -4 -cf /tmp/dhclient.conf {job.arg_1} && "
         + "ip route show && rm -f /tmp/dhclient.conf"
     )
     info(out)
@@ -432,8 +432,13 @@ def dhcp_server(job: Job, job_host):
     ip_range_end = job.arg_2
     mask = job.arg_3
     gw = job.arg_4
+    intfs = [job.arg_5]
     daemon = Dnsmasq(
-        node=job_host, ip_range=f"{ip_range_start},{ip_range_end}", mask=mask, gw=gw
+        node=job_host,
+        ip_range=f"{ip_range_start},{ip_range_end}",
+        mask=mask,
+        gw=gw,
+        intfs=intfs,
     )
     job_host.build_daemon(daemon)
     job_host.start_daemon(daemon)
